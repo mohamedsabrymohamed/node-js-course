703ea06d6363f26ca50b0c3864359d4a3a1b4224
Mohamed Sabry <Mohamed.Sabry@CNDC.LOCAL>
2020-07-08 09:59:50 +0200

    remove node-modules folder

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,7 @@
-const validator = require('validator')
-const getter = require('./notes.js')
-
-const display = getter('test')
-
-console.log(display)
+const chalk = require('chalk');
+ 
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+ 
+console.log(error('Error!'));
+console.log(warning('Warning!'));
