74a6cbd339d65cfed0e674310e7397856277610d
Mohamed Sabry <Mohamed.Sabry@CNDC.LOCAL>
2020-07-09 10:00:13 +0200

    add index html and routes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,12 +1,22 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
-//app.get used as routes
-app.get('',(req,res) => {
-    res.send('Hello')
-})
+// define public folder
+const publicDirectoryPath = path.join(__dirname,'../public')
 
+//to use a static page with default route
+app.use(express.static(publicDirectoryPath))
+
+//app.get used as routes
+// app.get('',(req,res) => {
+//     res.send('Hello')
+// })
+
+app.get('/help',(req,res) => {
+    res.send('help page')
+})
 
 
 app.listen(3000, ()=>{
