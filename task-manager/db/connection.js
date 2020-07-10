c4a2a982db2ccbc0f1dcc58a5039094627b836d0
mohamed sabry <mohamed.sabry@mail.com>
2020-07-10 18:55:20 +0200

    try to fix mysql issue

--- task-manager/db/connection.js
+++ task-manager/db/connection.js
@@ -1,15 +1,15 @@
 const host = 'localhost'
 const username = 'root'
-const password = ''
+const password = '123456'
 const databse_name = 'task_manager'
 
-const mysql      = require('mysql');
+const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host     : host,
-  user     : username,
-  password : password,
-  database : databse_name
+  host: host,
+  user: username,
+  password: password,
+  database: databse_name
 });
- 
+
 
 module.exports = connection
