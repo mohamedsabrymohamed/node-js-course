3f1fd5101f6c39813615926a29928b407340d173
Mohamed Sabry <Mohamed.Sabry@CNDC.LOCAL>
2020-07-12 08:27:43 +0200

    finish resource reading in mongoose and express as api

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -37,6 +37,62 @@ app.post('/tasks', (req, res) => {
     })
 })
 
+
+////////////////////////// resource Reading //////////////////////////
+
+////////////////////////// Users Model //////////////////////////
+////////////////////////// fetch all //////////////////////////
+
+app.get('/users',(req,res)=>{
+    User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+////////////////////////// fetch by id //////////////////////////
+
+app.get('/users/:id',(req,res)=>{
+    const _id = req.params.id
+    User.findById(_id).then((user)=>{
+        if(!user) {
+            return res.status(400).send()
+        }
+        res.send(user)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+
+////////////////////////// Tasks Model //////////////////////////
+////////////////////////// fetch all //////////////////////////
+
+app.get('/tasks',(req,res)=>{
+    User.find({}).then((tasks)=>{
+        res.send(tasks)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+////////////////////////// fetch by id //////////////////////////
+
+app.get('/tasks/:id',(req,res)=>{
+    const _id = req.params.id
+    User.findById(_id).then((task)=>{
+        if(!task) {
+            return res.status(400).send()
+        }
+        res.send(task)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+
+
 ////////////////////////// Express Listen Port //////////////////////////
 app.listen(port, () => {
     console.log('serve is up on  port ' + port)
