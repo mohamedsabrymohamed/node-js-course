229fb8f9ddd4c4ba02ca940d9a11366453fe8755
Mohamed Sabry <Mohamed.Sabry@CNDC.LOCAL>
2020-07-13 08:48:11 +0200

    install mariadb plugin and try connection and query with it

--- mysql_test/db/build_crud.js
+++ mysql_test/db/build_crud.js
@@ -1,36 +1,46 @@
 const connection = require('./connection');
 
-function get_info(coulmn_names, table_name, callback) {
-	var sql = 'SELECT ?? from ' + table_name;
+// function get_info(coulmn_names, table_name, callback) {
+// 	var sql = 'SELECT ?? from ' + table_name;
 
-	connection.query(
-		sql,
-		[
-			coulmn_names
-		],
-		function(err, results) {
-			if (err) {
-				throw err;
-			}
-			return callback(results);
-		}
-	);
-}
+// 	connection.query(
+// 		sql,
+// 		[
+// 			coulmn_names
+// 		],
+// 		function(err, results) {
+// 			if (err) {
+// 				throw err;
+// 			}
+// 			return callback(results);
+// 		}
+// 	);
+// }
 
-//usage
+// //usage
 
-var query_result = [];
-const columns = [
-	'id',
-	'username'
-];
-const table = 'test';
+// var query_result = [];
+// const columns = [
+// 	'id',
+// 	'username'
+// ];
+// const table = 'test';
 
-get_info(columns, table, function(result) {
-	//console.log(result);
-	query_result = result;
-});
+// get_info(columns, table, function(result) {
+// 	//console.log(result);
+// 	query_result = result;
+// });
 
-console.log(query_result);
+// console.log(query_result);
 
-connection.end();
+// connection.end();
+
+
+var result = connection.query("select * from test", async (err, rows, meta) => {
+	if (err) throw err;
+	console.log(rows); //[ { 'now()': 2018-07-02T17:06:38.000Z } ]
+	return rows
+  });
+
+  //console.log( result)
+  
--- mysql_test/db/connection.js
+++ mysql_test/db/connection.js
@@ -1,15 +1,30 @@
 const host = 'localhost'
 const username = 'root'
-const password = '123456'
+const password = ''
 const databse_name = 'task_manager'
 
-const mysql = require('mysql');
-const connection = mysql.createConnection({
-  host: host,
-  user: username,
+//const mysql = require('mysql');
+const mariadb = require('mariadb/callback');
+// const connection = mysql.createConnection({
+//   host: host,
+//   user: username,
+//   password: password,
+//   database: databse_name
+// });
+
+const conn = mariadb.createConnection({
+  host: host, 
+  user:username,
   password: password,
-  database: databse_name
+  database:databse_name
 });
+conn.connect(err => {
+if (err) {
+console.log("not connected due to error: " + err);
+} else {
+console.log("connected ! connection id is " + conn.threadId);
+}
+});
+ 
 
-
-module.exports = connection
+module.exports = conn
