ec7407e9301b4cf600ff04554138ba637304682a
Mohamed Sabry <Mohamed.Sabry@CNDC.LOCAL>
2020-07-13 08:22:09 +0200

    start testing socket io server client sides

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,12 +1,23 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.unsubscribe(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath))
 
-app.listen(port, ()=>{
+
+// listen for event on socket io
+io.on('connection',()=>{
+    console.log('New socket')
+})
+
+server.listen(port, ()=>{
     console.log(`Server is up on port ${port}`)
 })
